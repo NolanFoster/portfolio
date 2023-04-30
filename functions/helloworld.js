3f49b51175b3eef230923a9774cc2009a9e98ebc
nolan foster <nolanfoster@nolans-mbp.lan>
2023-04-30 16:11:47 -0400

    Updated helloworld

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,5 +1,9 @@
-export function onRequest(context) {
-    const value = context.env.PORTFOLIO.list();
-    return new Response(value.keys);
+export async function onRequest(context) {
+    const value = await context.env.PORTFOLIO.get("whoami");
+    if (value === null) {
+      return new Response("Value not found", { status: 404 });
+    }
+    console.log(value);
+    return new Response(value);
   }
   
