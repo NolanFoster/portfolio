36cf27f4a2bfc30c6098294c7423a643b635c01c
Nolan Foster <nolanfoster@gmail.com>
2023-05-25 00:52:28 +0000

    Updated header

--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -2,22 +2,49 @@ import React from 'react'
 import './header.css'
 import Typewriter from "typewriter-effect";
 
-function Header() {
-  return (
-    <div className='main-info'>
-      <h1>What am I?</h1>
-      <Typewriter
 
-        options={{
-            strings: ['A Software Engineer', 'Also known as an', "Back-end developer", "Embedded developer", 
-            "Mobile developer", "Or!", "<strong>a coder</strong>"],
-            autoStart: true,
-            loop: true,
-        }}
+
+const getHeaderData = async () => {
+  console.log("index.html 10 | Processing...");
+  const request = await fetch("hhttps://nolanfoster.me/whoami");
+  const data = await request.json();
+  return data;
+};
+
+const headerData = async() => await getHeaderData();
+
+function Header() {
+    return (
+      <div className='main-info'>
+        <h1>What am I?</h1>
+        <Typewriter
+  
+          options={{
+              strings: headerData.split(","),
+              autoStart: true,
+              loop: true,
+          }}
+          
+          />
+      </div>
+    )
+ }
+
+  //return (
+  //  <div className='main-info'>
+  //    <h1>What am I?</h1>
+  //    <Typewriter
+
+  //      options={{
+  //          strings: ['A Software Engineer', 'Also known as an', "Back-end developer", "Embedded developer", 
+  //          "Mobile developer", "Or!", "<strong>a coder</strong>"],
+  //          autoStart: true,
+  //          loop: true,
+  //      }}
         
-        />
-    </div>
-  )
-}
+  //      />
+  //  </div>
+//  )
+//}
 
 export default Header
