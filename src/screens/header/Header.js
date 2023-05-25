823fc60947a9cee5b247ed6606924b92be6739f1
nolan foster <nolanfoster@nolans-mbp.lan>
2023-05-24 22:11:12 -0400

    test strings call plain text

--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -6,16 +6,16 @@ import Typewriter from "typewriter-effect";
 
 const getHeaderData = async () => {
   const request = await fetch("https://nolanfoster.me/whoami");
-  const data = await request.json();
+  const data = await request.text();
   console.log(data);
   
-  return data.strinify(); // return success object
+  return data; // return success object
 };
 
 const headerData = async() => await getHeaderData();
 
 function Header() {
-    headerData();
+     headerData();
      return (
       <div className='main-info'>
         <h1>What am I?</h1>
