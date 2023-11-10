2d1f9eb424231cd41dfade2bf74aa072d53b6fca
nolan foster <nolanfoster@nolans-MacBook-Pro.local>
2023-11-09 19:38:35 -0800

    Removed module

--- functions/whoami.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-    const value = await context.env.PORTFOLIO.get("whoami");
-    if (value === null) {
-      return new Response("Value not found", { status: 404 });
-    }
-    console.log(value);
-    return new Response(value);
-  }
-  
