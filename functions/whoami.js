0b4a6cc73bff3edcaae4ab788422185eab1cfd5e
nolan foster <nolanfoster@nolans-MacBook-Pro.local>
2023-11-09 20:27:11 -0800

    Added back text function

---
+++ functions/whoami.js
@@ -0,0 +1,8 @@
+export async function onRequest(context) {
+    const value = await context.env.PORTFOLIO.get("whoami");
+    if (value === null) {
+      return new Response("Value not found", { status: 404 });
+    }
+    return new Response(value);
+  }
+  
